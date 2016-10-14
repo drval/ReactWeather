b35849c765c51ab86d29bef119109e9759f36cc1
drval <rudrval@gmail.com>
2016-10-15 01:02:36 +0400

    es6 support

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var o1 = {
+    name: 'drval',
+    location: 'togliatti'
+};
+
+var o2 = {
+    age: 37,
+    ...o1
+};
+
+console.log(o2);
+
 ReactDOM.render(
     <h1>Boilerplate App!</h1>
     ,document.getElementById('app')
