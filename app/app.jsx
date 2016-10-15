65a94388c4ce7a6c85294501a2ecbb7752452ab6
drval <rudrval@gmail.com>
2016-10-16 02:21:26 +0400

    remove _k from url

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {createHashHistory} = require('history');
+var {Route, Router, IndexRoute, useRouterHistory} = require('react-router');
+
+const hashHistory = useRouterHistory(createHashHistory)({queryKey: false});
 
 var Main = require('Main');
 var Weather = require('Weather');
